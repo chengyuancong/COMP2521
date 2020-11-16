7b9e2ef0a74e359f462d7ae80f64cd32e2a9ac85
chengyuancong <chengyuantsung@gmail.com>
2020-11-17 00:17:35 +1100

    Complete listIsOrdered

--- practice_exercises/linked_lists/1_listIsOrdered/listIsOrdered.c
+++ practice_exercises/linked_lists/1_listIsOrdered/listIsOrdered.c
@@ -2,7 +2,34 @@
 #include "list.h"
 
 bool listIsOrdered(List l) {
-	// TODO
-	return false;
+	Node head = l->head;
+	if (head == NULL || head->next == NULL || head->next->next == NULL) {
+		return true;
+	} else {
+		Node curr = head;
+		Node next = head->next;
+		int seq = 0;
+		while (next != NULL) {
+			if (curr->value != next->value) {
+				seq = next->value - curr->value;		// > 0 if assending, < 0 dessending
+				break;
+			}
+			curr = curr->next;
+			next = next->next;
+		}
+		if (seq == 0) {						// all equal
+			return true;
+		} else {							// not all equal
+			while (next != NULL) {
+				if (curr->value != next->value && (next->value - curr->value)*seq < 0) {
+					return false;
+				}
+				curr = curr->next;
+				next = next->next;
+			}
+		}
+		return true;
+	}
 }
+
 

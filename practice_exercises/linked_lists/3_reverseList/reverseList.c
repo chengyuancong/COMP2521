5d4d4ae2fd5bcdcd722d3a85ad15e37dedcc1ce2
chengyuancong <chengyuantsung@gmail.com>
2020-12-02 17:54:18 +1100

    Refactored iteration and add recursive version

--- practice_exercises/linked_lists/3_reverseList/reverseList.c
+++ practice_exercises/linked_lists/3_reverseList/reverseList.c
@@ -1,20 +1,40 @@
 
 #include "list.h"
 
+
+static Node doListReverse(Node head);
+
 void listReverse(List l) {
 	Node head = l->head;
 	if (head != NULL && head->next != NULL) {
-		// use first node as tail
-		Node newList = head;
-		Node remained = head->next;
-		newList->next = NULL;
-		while (remained != NULL) {
-			Node newHead = remained;
-			remained = remained->next;
-			newHead->next = newList;
-			newList = newHead;
-		}
-		l->head = newList;
+		l->head = doListReverse(head);
 	}
 }
 
+static Node doListReverse(Node head) {
+	if (head->next == NULL) {
+		return head;
+	}
+	Node last = doListReverse(head->next);
+	head->next->next = head;
+	head->next = NULL;
+	return last;
+}
+
+
+// iteratively
+/* void listReverse(List l) {
+	Node head = l->head;
+	if (head != NULL && head->next != NULL) {
+		Node curr = l->head;
+		Node prev = NULL;
+		Node remained = NULL;
+		while (curr != NULL) {
+			remained = curr->next;
+			curr->next = prev;
+			prev = curr;
+			curr = remained;
+		}
+		l->head = prev;
+	}
+} */

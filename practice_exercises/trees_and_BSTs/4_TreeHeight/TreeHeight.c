8f9f996e6f6ff8d45fe8e83ac72b8cbf915e61d7
chengyuancong <chengyuantsung@gmail.com>
2020-12-04 08:56:26 +1100

    Simplified conditions

--- practice_exercises/trees_and_BSTs/4_TreeHeight/TreeHeight.c
+++ practice_exercises/trees_and_BSTs/4_TreeHeight/TreeHeight.c
@@ -3,15 +3,15 @@
 
 #include "tree.h"
 
+static int max(int a, int b);
+
 int TreeHeight(Tree t) {
     if (t == NULL) {
         return -1;
     }
-    int heightL = TreeHeight(t->left);
-    int heightR = TreeHeight(t->right);
-    if (heightL > heightR) {
-        return heightL + 1;
-    } else {
-        return heightR + 1;
-    }
+    return max(TreeHeight(t->left), TreeHeight(t->right))+1;
+}
+
+static int max(int a, int b) {
+    return a > b ? a : b;
 }

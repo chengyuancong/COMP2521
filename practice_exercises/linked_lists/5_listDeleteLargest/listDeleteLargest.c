199ea7176373d1fdd757cb3027b23fd7f0a83c6a
chengyuancong <chengyuantsung@gmail.com>
2020-12-04 00:16:35 +1100

    Improved from two pass to one pass

--- practice_exercises/linked_lists/5_listDeleteLargest/listDeleteLargest.c
+++ practice_exercises/linked_lists/5_listDeleteLargest/listDeleteLargest.c
@@ -4,22 +4,21 @@
 int listDeleteLargest(List l) {
 	Node head = l->head;
 	Node max = head;
+	Node currPrev = NULL;
+	Node maxPrev = NULL;
 	int num = 0;
-	for (Node curr = head; curr != NULL; curr = curr->next) {
+	for (Node curr = head; curr != NULL; currPrev = curr, curr = curr->next) {
 		if (curr->value > max->value) {
 			max = curr;
+			maxPrev = currPrev;
 		}
 	}
-	if (max == head) {
+	if (maxPrev == NULL) {
 		num = head->value;
 		l->head = head->next;
 		free(head);
 	} else {
-		Node prev = head;
-		while (prev->next != max) {
-			prev = prev->next;
-		}
-		prev->next = max->next;
+		maxPrev->next = max->next;
 		num = max->value;
 		free(max);
 	}
